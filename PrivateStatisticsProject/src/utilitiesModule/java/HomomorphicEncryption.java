862ac24c4d2871db460118ce52e1f49aeeb100ff
Iulia Madalina Obreja <Iulia Madalina Obreja>
2016-11-07 21:53:00 +0200

    Project skeleton created

---
+++ PrivateStatisticsProject/src/utilitiesModule/java/HomomorphicEncryption.java
@@ -0,0 +1,14 @@
+package utilitiesModule.java;
+
+public class HomomorphicEncryption implements DataEncryption{
+
+	@Override
+	public void encryptData(){
+		
+	}
+	
+	@Override
+	public void decryptData(){
+		
+	}
+}
