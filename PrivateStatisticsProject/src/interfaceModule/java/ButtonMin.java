34ed2b320381d0693ecebae3feab17d1c3dc697f
andreibarbuta8 <andrei.barbuta@yahoo.com>
2016-11-08 08:07:18 +0200

    add ifmodule

---
+++ PrivateStatisticsProject/src/interfaceModule/java/ButtonMin.java
@@ -0,0 +1,18 @@
+package interfaceModule.java;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import interfaceModule.java.Buttons;
+/**
+ *
+ * @author jakal
+ */
+public class ButtonMin extends Buttons implements ButtonsInterface{
+    public ButtonMin(){
+    }
+    public void execute(){
+    }
+}
---
+++ PrivateStatisticsProject/src/interfaceModule/java/Buttons.java
@@ -0,0 +1,23 @@
+package interfaceModule.java;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JButton;
+/**
+ *
+ * @author jakal
+ */
+
+public class Buttons extends JButton implements ButtonsInterface {
+
+	public Buttons() {		
+        }        
+        public void setDefaultValues(){
+        } 
+        
+        public void execute() {
+	}
+}
