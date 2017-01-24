573b440ca9bb8b32c9dd4883d51a75261f2a68b2
BurgheleaDragos <burghelea.dragos@gmail.com>
2017-01-24 19:34:30 +0200

    moved clientuser and user from main to individual classes.

---
+++ PrivateStatisticsProject/PrivateStatistics/src/databaseModule/java/User.java
@@ -0,0 +1,12 @@
+package databaseModule.java;
+
+import java.math.BigInteger;
+
+import queriesModule.java.UserLevelKeyLength;
+
+public class User {
+	String Name;
+	BigInteger Age;
+	String UserName;
+	UserLevelKeyLength level;
+}
---
+++ PrivateStatisticsProject/PrivateStatistics/src/queriesModule/java/ClientUser.java
@@ -0,0 +1,43 @@
+package queriesModule.java;
+
+import java.math.BigInteger;
+
+public class ClientUser{
+	public ClientUser() {}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public BigInteger getP() {
+		return p;
+	}
+	public void setP(BigInteger p) {
+		this.p = p;
+	}
+	public BigInteger getQ() {
+		return q;
+	}
+	public void setQ(BigInteger q) {
+		this.q = q;
+	}
+	public int getCertainty() {
+		return Certainty;
+	}
+	public void setCertainty(int certainty) {
+		Certainty = certainty;
+	}
+	public int getKeyLength() {
+		return KeyLength;
+	}
+	public void setKeyLength(int keyLength) {
+		KeyLength = keyLength;
+	}
+	private String username;
+	private BigInteger p;
+	private BigInteger q;
+	private int Certainty;
+	private int KeyLength;
+}
